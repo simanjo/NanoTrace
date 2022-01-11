1be18f30bd0c9951898fd8caf14f8d93bf84cf8b
DESKTOP-IL0J6VQ\Jonny <82642377+simanjo@users.noreply.github.com>
2022-01-11 12:02:35 +0100

    reworked active channel calculation and introduced seperat squiggle plot window

--- gui.py
+++ gui.py
@@ -5,21 +5,21 @@ import numpy as np
 def _is_active_channel(fname, channel):
     with BulkFast5(fname) as fh:
         raw_data = fh.get_raw(channel)
-    baseline = None
 
-    try:
-        if np.abs(np.mean(raw_data)) > 1:
-            baseline = int(np.median(raw_data[np.logical_and(raw_data > 150, raw_data < 350)]))
-    finally:
-        return (baseline is not None)
+    return np.abs(np.mean(raw_data)) > 1 and len(raw_data[np.logical_and(raw_data > 150, raw_data < 350)]) > 0
+    # baseline = None
+
 
 
 def _get_active_channels(fname):
-    return [str(c) for c in range(1,127) if _is_active_channel(fname, c)]
+    result = [
+        c for c in range(1,127) if _is_active_channel(fname, c)
+    ]
+    return result
 
 def set_active_channels():
-    fname = dpg.get_value("file")
-    if fname is None:
+    fname = dpg.get_value("filepath")
+    if not fname:
         return
 
     chans = _get_active_channels(fname)
@@ -28,15 +28,21 @@ def set_active_channels():
 
 
 def choose_file(sender, app_data, user_data):
-    fname = list(app_data['selections'].values())[0]
+    try:
+        fpath = list(app_data['selections'].values())[0]
+        fname = list(app_data['selections'].keys())[0]
+    except KeyError:
+        return
     dpg.set_value("file", fname)
-    dpg.configure_item("channel", items=[str(c) for c in range(1,127)])
+    dpg.set_value("filepath", fpath)
+    dpg.configure_item("channel", items=list(range(1,127)))
 
 
-def choose_channel():
-    c = int(dpg.get_value("channel"))
-    fname = dpg.get_value("file")
-    if fname is None or c is None:
+
+def choose_channel(sender, app_data, user_data):
+    c = dpg.get_value("channel")
+    fname = dpg.get_value("filepath")
+    if not fname or not c:
         return
 
     with BulkFast5(fname) as fh:
@@ -55,31 +61,37 @@ def main():
         dpg.add_file_extension(".*")
         dpg.add_file_extension(".fast5", color=(0, 255, 255, 255), custom_text="[fast5]")
 
+    with dpg.value_registry():
+        dpg.add_string_value(tag="filepath")
 
-    with dpg.window(label="Raw Data", width=800, height=600):
+    with dpg.window(label="Command Central", autosize=True, no_close=True, no_collapse=True):
         with dpg.group(horizontal=True):
             dpg.add_button(label="File Selector", callback=lambda: dpg.show_item("file_dialog"))
-            dpg.add_text("Selected File: ", tag="file")
+            dpg.add_text(tag="file", show=False)
         with dpg.group(horizontal=True):
-            dpg.add_combo(tag="channel", callback=choose_channel)
+            dpg.add_text("Channel:")
+            dpg.add_combo(tag="channel", width=60)
             dpg.add_button(label="Get active channels", callback=set_active_channels)
+        with dpg.group(tag="func_choose"):
+            dpg.add_button(label="Show Squiggle Plot", callback=choose_channel)
 
+    with dpg.window(label="Raw Data", width=800, height=600, tag="raw-data"):
         with dpg.plot(label="Squiggle Plot", height=-1, width=-1):
             dpg.add_plot_legend()
 
             # create x axis
-            dpg.add_plot_axis(dpg.mvXAxis, label="index", no_gridlines=True, tag="x_axis")
+            dpg.add_plot_axis(dpg.mvXAxis, label="index", no_gridlines=True, tag="raw_x_axis")
             dpg.set_axis_limits(dpg.last_item(), 0, 100000)
             # dpg.set_axis_ticks(dpg.last_item(), (("S1", 11), ("S2", 21), ("S3", 31)))
 
             # create y axis
-            dpg.add_plot_axis(dpg.mvYAxis, label="current [pA]", tag="y_axis")
-            dpg.set_axis_limits("y_axis", -20, 350)
+            dpg.add_plot_axis(dpg.mvYAxis, label="current [pA]", tag="raw_y_axis")
+            dpg.set_axis_limits(dpg.last_item(), -20, 350)
 
             # add series to y axis
             dpg.add_line_series([], [], parent="y_axis", tag = "raw_series")
 
-    dpg.create_viewport(title='NanoTrace', width=800, height=1000)
+    dpg.create_viewport(title='NanoTrace', width=850, height=800)
     dpg.setup_dearpygui()
 
     dpg.show_viewport()
