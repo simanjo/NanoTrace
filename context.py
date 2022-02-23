f9e5ca9697eb66b346507b6ba240e0c112f4014e
DESKTOP-IL0J6VQ\Jonny <82642377+simanjo@users.noreply.github.com>
2022-02-23 16:47:53 +0100

    WIP: added update_context for adding of experiments to context

--- context.py
+++ context.py
@@ -1,13 +1,19 @@
-from typing import Dict, Any, List, Optional
+from typing import Dict, Any, List, Optional, Union
+from os import path
 
-from Experiment import Experiment
-from utils import get_active_channels
+import dearpygui.dearpygui as dpg
+
+from experiment import Experiment
+from utils import get_active_channels, parse_exp_name
+from python_toolbox.util import get_file_hash
 
 ## HACK
 DEFAULT_SETTINGS = {
     'kde_resolution': 1_000_000,
     'burnin': 350_000
 }
+
+DpgItem = Union[int, str]
 
 class Context:
 
@@ -21,8 +27,29 @@ class Context:
         self.settings: Dict[str, Any] = settings
         self.active_exp: Optional[Experiment] = None
 
-    def _get_active_exp(self) -> Experiment:
-        return self.active_exp
+    def update_context(
+        self, fpath: str, progressbar: Optional[DpgItem] = None
+    ) -> None:
+        # TODO: add sanity checks for file values
+        # calculate hash first and check if experiment is known
+        if progressbar is not None:
+            dpg.configure_item(
+                progressbar, show=True, overlay="Calculating file hash"
+            )
+            dpg.set_value(progressbar, 0.5)
+        hash = get_file_hash(fpath, algo='blake2b')
+        if progressbar is not None:
+            dpg.configure_item(progressbar, show=False)
+
+        if hash not in self.exps.keys():
+            # TODO: add different hash algorithms?
+            fname = path.split(fpath)[1]
+            properties = parse_exp_name(fname)
+            exp = Experiment(fname, fpath, {'blake2b': hash}, None, properties)
+            self.exps[hash] = exp
+        else:
+            exp = self.exps[hash]
+        self.active_exp = exp
 
     def get_burnin() -> int:
         return DEFAULT_SETTINGS['burnin']
