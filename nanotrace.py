e9ad12ec63ae81447a6db91911d0d97b8c7117ff
DESKTOP-IL0J6VQ\Jonny <82642377+simanjo@users.noreply.github.com>
2022-02-23 16:19:51 +0100

    removed md5 calculation function, now in python_toolbox

--- nanotrace.py
+++ nanotrace.py
@@ -9,13 +9,6 @@ import random
 
 
 
-def md5(fname):
-    hash_md5 = hashlib.md5()
-    with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
-
 def set_active_channels():
     global exp_df
     # first thing to happen: button vanishes
