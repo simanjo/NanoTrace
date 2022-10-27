4868a2d7497da2b3da77ef121de0d6b9f3360b3f
DESKTOP-IL0J6VQ\Jonny <82642377+simanjo@users.noreply.github.com>
2022-10-27 20:09:04 +0200

    added functions to extract mean baseline/events for experiments

--- experiment.py
+++ experiment.py
@@ -1,5 +1,9 @@
 from typing import Any, Literal, Dict, Optional
 
+from numpy import mean, median, std
+
+from utils import event_density
+
 
 class Experiment:
 
@@ -26,3 +30,38 @@ class Experiment:
         if self.band_distribution is None:
             return None
         return list(self.band_distribution.keys())
+
+    def get_mean_baselines(self, filter_mad=True):
+        if self.band_distribution is None:
+            return None
+
+        baselines = [val[0] for val in self.band_distribution.values()]
+        if filter_mad:
+            bl_median = median(baselines)
+            bl_mad = median([abs(bl - bl_median) for bl in baselines])
+            baselines = [
+                bl for bl in baselines if abs(bl - bl_median) < 3*bl_mad
+            ]
+        return (round(mean(baselines), 2), round(std(baselines), 3))
+
+    def get_mean_events(self, filter_mad=True):
+        if self.band_distribution is None:
+            return None
+
+        if filter_mad:
+            baselines = {k: v[0] for k, v in self.band_distribution.items()}
+            bl_median = median(list(baselines.values()))
+            bl_mad = median([abs(bl - bl_median) for bl in baselines.values()])
+            indizes = [
+                k for k, bl in baselines.items()
+                if abs(bl - bl_median) < 3*bl_mad
+            ]
+            events = [
+                event_density(v[1]) for k, v in self.band_distribution.items()
+                if k in indizes
+            ]
+        else:
+            events = [
+                event_density(v[1]) for v in self.band_distribution.values()
+            ]
+        return (round(mean(events), 4), round(std(events), 3))
